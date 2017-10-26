46444ed56d5312001f36a53c2733973762d7665d
guangqiang <1126756952@qq.com>
2017-10-26 20:12:57 +0800

    添加样式表适配组件

--- App.js
+++ App.js
@@ -1,57 +1,51 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * StyleSheetDemo
+ * https://github.com/guangqiang-liu
+ * @guangqiang：qq交流群：620792950
  */
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet} from './StyleSheet'
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.box}></View>
+        <Text style={styles.textStyle}>iOS、Android样式适配组件讲解</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  box: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'red',
+    _height: 300,
+    _width: 300,
+    _backgroundColor: 'orange',
+    _borderRadius: 150
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  textStyle: {
+    color: 'green',
+    _color: 'blue',
+    fontSize: 15,
+    _fontSize: 25,
+    _backgroundColor: 'red',
+    marginTop: 10,
+    _marginTop: 100
+  }
+})
