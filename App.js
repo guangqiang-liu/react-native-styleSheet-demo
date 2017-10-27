3e71b2886a9ed348757070ec245842dc1bc3dea7
guangqiang <1126756952@qq.com>
2017-10-27 15:34:31 +0800

    修改作者信息和样式

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 /**
  * StyleSheetDemo
- * https://github.com/guangqiang-liu
- * @guangqiang：qq交流群：620792950
+ * 作者Git：https://github.com/guangqiang-liu
+ * 技术交流群：620792950
+ * 作者QQ：1126756952
+ * @guangqiang
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Text,
-  View
+  View,
+  Platform
 } from 'react-native'
+
+// import {StyleSheet} from 'react-native'
 
 import {StyleSheet} from './StyleSheet'
 
@@ -16,8 +21,14 @@ export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.box}></View>
         <Text style={styles.textStyle}>iOS、Android样式适配组件讲解</Text>
+        <View style={styles.box}>
+          <Text>iOS平台是正方形，Android平台是圆形</Text>
+        </View>
+        <Text style={{marginTop: 20}}>使用Platform判断平台</Text>
+        <View style={styles.platform}>
+          <Text>这时使用原始的Platform判断</Text>
+        </View>
       </View>
     )
   }
@@ -32,12 +43,14 @@ const styles = StyleSheet.create({
   },
   box: {
     height: 100,
+    _height: 200,
     width: 100,
+    _width: 200,
     backgroundColor: 'red',
-    _height: 300,
-    _width: 300,
     _backgroundColor: 'orange',
-    _borderRadius: 150
+    _borderRadius: 200,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   textStyle: {
     color: 'green',
@@ -45,7 +58,16 @@ const styles = StyleSheet.create({
     fontSize: 15,
     _fontSize: 25,
     _backgroundColor: 'red',
-    marginTop: 10,
-    _marginTop: 100
+    marginBottom: 10,
+    _marginBottom: 100
+  },
+  platform: {
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: Platform.OS === 'ios' ? 120 : 100,
+    height: Platform.OS === 'ios' ? 120 : 100,
+    borderRadius: Platform.OS === 'ios' ? 60 : 30,
+    backgroundColor: Platform.OS === 'ios' ? 'red' : 'green'
   }
 })
